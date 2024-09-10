41d20855859294dc721692c5f53769dc2301b04a
BigshotKun1 <vicente.castillo2001@alumnos.ubiobio.cl>
2024-09-10 17:05:52 -0300

    ADD Operadores Aritmeticos

---
+++ Operadores Aritmeticos.js
@@ -0,0 +1,27 @@
+const numero1 = 22;
+const numero2 = 18;
+
+function sumar(numero1, numero2) {
+    return numero1 + numero2;   
+}
+console.log("La suma es :", sumar(numero1, numero2));
+
+function restar(numero1, numero2) {
+    return numero1 - numero2;   
+}
+console.log("La resta es :", restar(numero1, numero2));
+
+function multiplicar(numero1, numero2) {  
+    return numero1 * numero2;
+}
+console.log("La multiplicación es :", multiplicar(numero1, numero2));
+
+function dividir(numero1, numero2) {
+    return numero1 / numero2;
+}   
+console.log("La división es :", dividir(numero1, numero2));
+
+function modulo(numero1, numero2) { 
+    return numero1 % numero2;
+}
+console.log("El módulo es :", modulo(numero1, numero2));
