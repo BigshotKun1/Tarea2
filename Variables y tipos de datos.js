834fe17a34cd59bccee4a89b46d7893689e94cb5
BigshotKun1 <vicente.castillo2001@alumnos.ubiobio.cl>
2024-09-10 16:59:10 -0300

    ADD Variables y tipos de datos

---
+++ Variables y tipos de datos.js
@@ -0,0 +1,12 @@
+const nombre = "Vicente";
+const edad = 22;
+const puedoConducir = true;
+
+console.log(nombre);
+console.log(edad);
+console.log(puedoConducir);
+
+console.log("Tipo de dato :", typeof nombre);
+console.log("Tipo de dato :", typeof edad);
+console.log("Tipo de dato :", typeof puedoConducir);
+
