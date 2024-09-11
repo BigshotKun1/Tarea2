b12f36983e1ec2b78a14c6996788095f97746c75
BigshotKun1 <vicente.castillo2001@alumnos.ubiobio.cl>
2024-09-10 21:31:21 -0300

    ADD Condicionales

---
+++ Condicionales.js
@@ -0,0 +1,68 @@
+const numero1 = 10;
+const numero2 = 20;
+const numero3 = 30;
+
+if (numero1 >= numero2 ){
+    console.log("numero1 es mayor que numero2 ");
+}else if (numero2 >= numero3){
+    console.log("numero2 es mayor que numero3 ");
+}else{
+    console.log("numero3 es mayor que numero2 ");
+}
+
+// imprimir por consola el numero menor de los tres     
+if (numero1 <= numero2 ){
+    console.log("numero1 es menor que numero2 ");
+}   else if (numero2 <= numero3){
+    console.log("numero2 es menor que numero3 ");
+}   else{
+    console.log("numero3 es menor que numero2 ");
+}   
+
+//Imprimir en consola si el numero1 es par o impar.
+
+if (numero1 % 2 == 0){
+    console.log("numero1 es par");}
+    else{
+        console.log("numero1 es impar");
+    }
+
+//Imprimir en consola si el numero2 es par o impar.
+
+if(numero2 % 2 == 0){
+    console.log("Numero2 es par");
+}else{
+    console.log("Numero2 es impar");
+}
+
+//Imprimir en consola si el numero3 es par o impar.
+
+if(numero3 % 2 == 0){
+    console.log("Numero3 es par");}
+    else{
+        console.log("Numero3 es impar");
+    }   
+
+//Imprimir en consola si el numero1 es múltiplo de 5.
+
+if(numero1 % 5 == 0){
+    console.log("Numero1 es multiplo de 5");
+    }else{
+        console.log("Numero1 no es multiplo de 5"); 
+    }
+
+//Imprimir en consola si el numero2 es múltiplo de 5.
+
+if (numero2 % 5 == 0){
+    console.log("Numero2 es multiplo de 5");
+    }else{
+        console.log("Numero2 no es multiplo de 5");
+    }
+
+//Imprimir en consola si el numero3 es múltiplo de 5.
+
+if (numero3 % 5 == 0){
+    console.log("Numero3 es multiplo de 5");
+}else {
+    console.log("Numero3 no es multiplo de 5"); 
+}
